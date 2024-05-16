a769698de39233b5c1a1337bd209e2bc0401b23a
Giffulf <146553362+Giffulf@users.noreply.github.com>
2024-05-16 22:03:22 +0300

    main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Depends
+import uvicorn
+from fastapi import FastAPI
 from src.urls import register_routers
 
 
-app = FastAPI(title="Twitter", debug=True, dependencies=[Depends()])
+app = FastAPI(title="Blogs", debug=True)
 
 register_routers(app)
+
+uvicorn.run(app, port=8000)
