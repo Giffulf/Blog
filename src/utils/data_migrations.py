a701282964fc51d02e379926486a62b84c29611c
Giffulf <146553362+Giffulf@users.noreply.github.com>
2024-05-29 22:16:10 +0300

    data_migrations.py

--- src/utils/data_migrations.py
+++ src/utils/data_migrations.py
@@ -9,23 +9,67 @@ from src.models.likes import Like
 users = [
     {
         "username": "Юля",
-        "api_key": "test1",
+        "api_key": "test",
     },
     {
         "username": "Катя",
         "api_key": "test2",
-    }
+    },
+    {
+        "username": "Лёша",
+        "api_key": "test3",
+    },
 ]
 
 posts = [
     {
-        "post_data": "Всем привет!! Я Юля!!!",
+        "post_data": "Как я устала от этой учёбы!",
         "user_id": 1,
     },
     {
-        "post_data": "Всем привет!! Я Катя!!!",
+        "post_data": "Кинофестиваль в Чите 31 мая - 2 июня",
         "user_id": 2,
-    }
+    },
+    {
+        "post_data": "Новейшая разработка в медицине",
+        "user_id": 3,
+    },
+    {
+        "post_data": "Игра года 20-24!",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Капец вы все веселые.. ха-ха.. где петля?",
+        "user_id": 1,
+    },
+    {
+        "post_data": "Да ладно, мне не жалко!",
+        "user_id": 1,
+    },
+    {
+        "post_data": "Угодовая косметика от Exile!",
+        "user_id": 3,
+    },
+    {
+        "post_data": "День рождения МИИГАиК 245 лет!",
+        "user_id": 3,
+    },
+    {
+        "post_data": "Как перебороть лень!",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Как поднять мотивацию?",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Новое обновление Геншина!!",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Да-да, я Максим...",
+        "user_id": 1,
+    },
 ]
 
 likes = [
@@ -34,11 +78,62 @@ likes = [
         "posts_id": 1,
     },
     {
+        "user_id": 3,
+        "posts_id": 1,
+    },
+    {
         "user_id": 2,
         "posts_id": 1,
-    }
+    },
+    {
+        "user_id": 2,
+        "posts_id": 2,
+    },
+    {
+        "user_id": 3,
+        "posts_id": 2,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 3,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 3,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 4,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 6,
+    },
+    {
+        "user_id": 3,
+        "posts_id": 6,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 7,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 9,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 9,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 11,
+    },
+    {
+        "user_id": 3,
+        "posts_id": 1,
+    },
 ]
-
 
 async def re_creation_db():
     """
