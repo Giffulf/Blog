1ed6d2014a7e1e2639a671ffaabbce91da109c90
Giffulf <146553362+Giffulf@users.noreply.github.com>
2024-05-29 22:17:57 +0300

    user.py

--- src/utils/user.py
+++ src/utils/user.py
@@ -1,35 +1,181 @@
-from fastapi import Security
-from http import HTTPStatus
 from loguru import logger
+import asyncio
 
-from src.database import async_session_maker
+from src.database import async_session_maker, engine, Base
 from src.models.users import User
-from src.services.user import UserService
-from src.utils.exeptions import CustomApiException
-from src.utils.token_my import TOKEN
+from src.models.posts import Post
+from src.models.likes import Like
 
+users = [
+    {
+        "username": "Юля",
+        "api_key": "test",
+    },
+    {
+        "username": "Катя",
+        "api_key": "test2",
+    },
+    {
+        "username": "Лёша",
+        "api_key": "test3",
+    },
+]
 
-async def get_current_user(token: str = Security(TOKEN)) -> User | None:
+posts = [
+    {
+        "post_data": "Как я устала от этой учёбы!",
+        "user_id": 1,
+    },
+    {
+        "post_data": "Кинофестиваль в Чите 31 мая - 2 июня",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Новейшая разработка в медицине",
+        "user_id": 3,
+    },
+    {
+        "post_data": "Игра года 20-24!",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Капец вы все веселые.. ха-ха.. где петля?",
+        "user_id": 1,
+    },
+    {
+        "post_data": "Да ладно, мне не жалко!",
+        "user_id": 1,
+    },
+    {
+        "post_data": "Угодовая косметика от Exile!",
+        "user_id": 3,
+    },
+    {
+        "post_data": "День рождения МИИГАиК 245 лет!",
+        "user_id": 3,
+    },
+    {
+        "post_data": "Как перебороть лень!",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Как поднять мотивацию?",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Новое обновление Геншина!!",
+        "user_id": 2,
+    },
+    {
+        "post_data": "Да-да, я Максим...",
+        "user_id": 1,
+    },
+]
+
+likes = [
+    {
+        "user_id": 1,
+        "posts_id": 1,
+    },
+    {
+        "user_id": 3,
+        "posts_id": 1,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 1,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 2,
+    },
+    {
+        "user_id": 3,
+        "posts_id": 2,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 3,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 3,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 4,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 6,
+    },
+    {
+        "user_id": 3,
+        "posts_id": 6,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 7,
+    },
+    {
+        "user_id": 1,
+        "posts_id": 9,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 9,
+    },
+    {
+        "user_id": 2,
+        "posts_id": 11,
+    },
+    {
+        "user_id": 3,
+        "posts_id": 1,
+    },
+]
+
+async def re_creation_db():
     """
-    Поиск и возврат пользователя из базы данных по токену из header
+    Удаление и создание БД
     """
+    logger.debug("Создание БД")
 
-    if token is None:
-        logger.error("Токен не найден в header")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)  # Удаление всех таблиц
+        await conn.run_sync(Base.metadata.create_all)  # Создание всех таблиц
 
-        raise CustomApiException(
-            status_code=HTTPStatus.UNAUTHORIZED,  # 401
-            detail="Valid api-token token is missing",
-        )
+
+async def migration_data():
+    """
+    Функция для наполнения БД демонстрационными данными
+    """
+    logger.debug("Загрузка демонстрационных данных")
+
+    await re_creation_db()
 
     async with async_session_maker() as session:
-        # Поиск пользователя
-        current_user = await UserService.get_user_for_key(token=token, session=session)
+        # Инициализируем и добавляем пользователей
+        initial_users = [User(**user) for user in users]
+        session.add_all(initial_users)
 
-        if current_user is None:
-            raise CustomApiException(
-                status_code=HTTPStatus.UNAUTHORIZED,  # 401
-                detail="Sorry. Wrong api-key token. This user does not exist",
-            )
+        # Подписки пользователей
+        initial_users[0].following.extend([initial_users[1], initial_users[2]])
+        initial_users[1].following.append(initial_users[0])
+        initial_users[2].following.extend([initial_users[1], initial_users[0]])
 
-        return current_user
+        # Посты
+        initial_posts = [Post(**post) for post in posts]
+        session.add_all(initial_posts)
+
+        # Лайки
+        initial_likes = [Like(**like) for like in likes]
+        session.add_all(initial_likes)
+
+        await session.commit()
+
+        logger.debug("Данные успешно добавлены")
+
+
+if __name__ == "__main__":
+    asyncio.run(migration_data())
